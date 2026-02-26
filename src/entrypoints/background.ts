dd2b8a93be2db657cd8ee72f95d9c3ff2e6c37d0
Zeke Barefoot <syncorex0@gmail.com>
2026-02-25 21:58:02 -0700

    fix ToC old link, refactor continuation req check

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -1,63 +1,60 @@
 export default defineBackground(() => {
-  console.log('[Background] Script loaded');
+	console.debug('[Background] Script loaded');
 
-  browser.webRequest.onBeforeRequest.addListener(
-    (details) => {
-      if (details.requestBody && details.requestBody.raw) {
-        const body = details.requestBody.raw[0].bytes;
-        if (body) {
-          const decoder = new TextDecoder();
-          const bodyStr = decoder.decode(body);
-          if (bodyStr.includes('"continuation"')) {
-            return { cancel: true };
-          }
-        }
-      }
-      return { cancel: false };
-    },
-    {
-      urls: ['https://www.youtube.com/youtubei/v1/browse'],
-    },
-    ['blocking', 'requestBody']
-  );
+	browser.webRequest.onBeforeRequest.addListener(
+		(details) => {
+			const body = details?.requestBody?.raw?.[0]?.bytes ?? new ArrayBuffer;
+			const decoder = new TextDecoder();
+			const bodyStr = decoder.decode(body);
 
-  browser.runtime.onMessage.addListener((message, _, sendResponse) => {
-    console.log('[Background] Received message:', message);
-    if (message.type === 'saveFilterSettings') {
-      console.log('[Background] Saving settings:', message.quantity, message.unit);
-      browser.storage.local.set({
-        filterQuantity: message.quantity,
-        filterUnit: message.unit
-      }).then(() => {
-        console.log('[Background] Settings saved, querying tabs...');
-        browser.tabs.query({ url: 'https://www.youtube.com/*' }).then((tabs) => {
-          console.log('[Background] Found tabs:', tabs.length);
-          for (const tab of tabs) {
-            browser.tabs.sendMessage(tab.id!, { type: 'filterSettingsChanged' });
-          }
-        });
-      }).catch(err => {
-        console.error('[Background] Error saving settings:', err);
-      });
-    }
-  });
+			if (bodyStr.includes('"continuation"'))
+				return { cancel: true };
+			else
+				return { cancel: false };
+		},
+		{
+			urls: ['https://www.youtube.com/youtubei/v1/browse'],
+		},
+		['blocking', 'requestBody']
+	);
 
-  browser.runtime.onMessage.addListener((message, _, sendResponse) => {
-    console.log('[Background] getFilterSettings request:', message);
-    if (message.type === 'getFilterSettings') {
-      browser.storage.local.get({
-        filterQuantity: '24',
-        filterUnit: 'hours'
-      }).then((result) => {
-        console.log('[Background] Returning settings:', result);
-        sendResponse({
-          quantity: result.filterQuantity as string,
-          unit: result.filterUnit as string
-        });
-      }).catch(err => {
-        console.error('[Background] Error getting settings:', err);
-      });
-      return true;
-    }
-  });
+	browser.runtime.onMessage.addListener((message, _, sendResponse) => {
+		console.log('[Background] Received message:', message);
+		if (message.type === 'saveFilterSettings') {
+			console.log('[Background] Saving settings:', message.quantity, message.unit);
+			browser.storage.local.set({
+				filterQuantity: message.quantity,
+				filterUnit: message.unit
+			}).then(() => {
+				console.log('[Background] Settings saved, querying tabs...');
+				browser.tabs.query({ url: 'https://www.youtube.com/*' }).then((tabs) => {
+					console.log('[Background] Found tabs:', tabs.length);
+					for (const tab of tabs) {
+						browser.tabs.sendMessage(tab.id!, { type: 'filterSettingsChanged' });
+					}
+				});
+			}).catch(err => {
+				console.error('[Background] Error saving settings:', err);
+			});
+		}
+	});
+
+	browser.runtime.onMessage.addListener((message, _, sendResponse) => {
+		console.log('[Background] getFilterSettings request:', message);
+		if (message.type === 'getFilterSettings') {
+			browser.storage.local.get({
+				filterQuantity: '24',
+				filterUnit: 'hours'
+			}).then((result) => {
+				console.log('[Background] Returning settings:', result);
+				sendResponse({
+					quantity: result.filterQuantity as string,
+					unit: result.filterUnit as string
+				});
+			}).catch(err => {
+				console.error('[Background] Error getting settings:', err);
+			});
+			return true;
+		}
+	});
 });
